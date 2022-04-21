73c5a42c32757443cf62e7e77d2afa66b1f48a61
Admin <kovalsky.valentine@gmail.com>
2022-04-21 15:08:59 +0300

    Start of JavaFX code

--- Pacific Ocean Economics Model/src/com/application/Main.java
+++ Pacific Ocean Economics Model/src/com/application/Main.java
@@ -1,8 +1,19 @@
 package com.application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.start();
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Model.fxml"));
+        Scene scene = new Scene(root);
     }
 }
